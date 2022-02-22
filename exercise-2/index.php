2ce27e9bc7de4cd201915a3d0828b956e2e9710a
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-02-22 15:04:14 +0100

    Add ucfirst() and small improvements echo styling

--- exercise-2/index.php
+++ exercise-2/index.php
@@ -12,7 +12,7 @@ require './Beer.php';
 // Instantiate an object which represents Duvel.
 // Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
 // Also the name equal to Duvel and the alcohol percentage to 8,5%
-$duvel = new Beer ('blond',3.5,'cold','Duvel',8.5);
+$duvel = new Beer ('blond',3.5,'cold','duvel',8.5);
 
 // Print the getAlcoholpercentage 2 times on the screen in two different ways:
 // 1) Print the getAlcoholpercentage and the color on the screen
@@ -22,9 +22,8 @@ $duvel = new Beer ('blond',3.5,'cold','Duvel',8.5);
 //Try to get this error on the screen:
 //Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholpercentage() in ... on line 64
 //USE TYPEHINTING EVERYWHERE!
-//echo $duvel:Beverage:: -> color;
 echo 'Result exercise 2:'.'<br><br>';
-echo $duvel -> getAlcPercentage().' - ';
+echo $duvel -> getAlcPercentage().'% - ';
 echo $duvel -> color.'<br>';
-echo $duvel -> getAlcPercentage().' - ';
+echo $duvel -> getAlcPercentage().'% - ';
 echo $duvel -> getInfo().'<br><br>';
