38167d72785c654cebf569175e809f3e68ba6fc4
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-02-22 14:34:16 +0100

    Improve comment styling

--- exercise-2/index.php
+++ exercise-2/index.php
@@ -9,13 +9,18 @@ error_reporting(E_ALL);
 require '../exercise-1/Beverage.php';
 require './Beer.php';
 
-// Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+// Instantiate an object which represents Duvel.
+// Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
 // Also the name equal to Duvel and the alcohol percentage to 8,5%
 $duvel = new Beer ('blond',3.5,'cold','Duvel',8.5);
 
-// Print the getAlcoholpercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+// Print the getAlcoholpercentage 2 times on the screen in two different ways:
+// 1) Print the getAlcoholpercentage and the color on the screen
+// 2) Print the getAlcoholpercentage and the getInfo on the screen
+
 //Make sure that each print is on a different line.
-//Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholpercentage() in /var/www/becode/workshop/exercise2.php on line 64
+//Try to get this error on the screen:
+//Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholpercentage() in ... on line 64
 //USE TYPEHINTING EVERYWHERE!
 //echo $duvel:Beverage:: -> color;
 echo 'Result exercise 2:'.'<br><br>';
